06c72ad839f5e7d75c36e1f56d558e35e5045e5b
Yuvraj Singh Jadon <yuvraajsj18@gmail.com>
2021-07-02 10:06:45 +0530

    removed cors

--- hello/main.py
+++ hello/main.py
@@ -5,29 +5,6 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-#################################################################
-# I have added this here because I used codespaces to develop the app
-# which make request from a different URL/origin, you won't find this in the
-# article. To learn about CORS and how to set it up in FastAPI:
-# https://fastapi.tiangolo.com/tutorial/cors/
-# you can ignore this, if you are using localhost.
-
-origins = [
-    "https://yuvraajsj18-todo-api-with-fastapi-xcj4-8000.githubpreview.dev",
-    "http://localhost",
-    "http://localhost:8080",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-#################################################################
-
 
 @app.get("/")
 async def hello():
