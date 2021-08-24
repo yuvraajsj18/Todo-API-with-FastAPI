0931ce8a8a0c19b81fbfede2534665a5b5b9a68c
Yuvraj Singh Jadon <yuvraajsj18@gmail.com>
2021-08-24 15:01:46 +0530

    fix: db url

--- todo/app/tests/conftest.py
+++ todo/app/tests/conftest.py
@@ -11,7 +11,7 @@ from sqlalchemy.orm import Session, sessionmaker
 
 from . import utils
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./app/test.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
